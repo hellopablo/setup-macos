0c27ad26e12fe5e1fbf5bc715572f648218d7914
Pablo de la Peña <p@shedcollective.org>
2016-12-29 09:47:19 -0600

    Adding SSH key to the keychain after generating it

--- configure/development.sh
+++ configure/development.sh
@@ -3,5 +3,8 @@ echo "Configuring the development environment..."
 echo "Development - Creating Sites folder"
 mkdir ~/Sites
 
-echo "Generating SSH keypair"
+echo "Generating SSH keypair (choose a secure password, we'll add it to the keychain next)"
 ssh-keygen
+
+echo "Adding key to the keychain"
+ssh-add
