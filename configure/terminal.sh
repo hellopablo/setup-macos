95f40593aa6aed01bf855ce90f04bd056683c1f4
Pablo de la Peña <p@shedcollective.org>
2016-12-29 17:36:13 -0600

    Terminal configurations

--- configure/terminal.sh
+++ configure/terminal.sh
@@ -1,4 +1,12 @@
 echo "Configuring Terminal..."
 
-# @todo - window size
-# @todo - theme
+echo "Terminal - Only use UTF-8 in Terminal.app"
+defaults write com.apple.terminal StringEncodings -array 4
+
+echo "Terminal - Window Size"
+defaults write com.apple.terminal rowCount -int 47
+defaults write com.apple.terminal columnCount -int 182
+
+echo "Terminal - Theme"
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
