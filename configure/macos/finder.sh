422d527f23c58b78d41947891f023cca225ea0c4
Pablo de la Peña <p@shedcollective.org>
2017-01-20 20:25:17 +0000

    Enable Snap to grid in Finder

--- configure/macos/finder.sh
+++ configure/macos/finder.sh
@@ -23,5 +23,10 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 echo "Finder - Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
+echo "Finder - Enable snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
 echo "Finder - Relaunching"
 killall Finder
