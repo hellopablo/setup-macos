93769fa5cce759fb6a23581efd5312ab99af0353
Pablo de la Peña <p@shedcollective.org>
2017-07-04 13:22:58 +0100

    Added Trello as a nativefier app

--- install/nativefier/nativefier.sh
+++ install/nativefier/nativefier.sh
@@ -21,6 +21,7 @@ makeNativefier "Shed Intranet" "https://intranet.studio.theshed.io"
 makeNativefier "Invision" "https://projects.invisionapp.com"
 makeNativefier "Dropbox Paper" "https://paper.dropbox.com"
 makeNativefier "Circle CI" "https://circleci.com/dashboard"
+makeNativefier "Trello" "https://trello.com/"
 
 find "$CACHE/nativefier" -name '*.app' -maxdepth 2 -exec cp -pr '{}' "/Applications/" ';'
 rm -rf "$CACHE/nativefier"
