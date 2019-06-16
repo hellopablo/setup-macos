f5bb81a371cf0c1409a88e79a561d82f57dd9b69
Pablo de la Peña <p@shedcollective.org>
2019-06-17 00:01:58 +0100

    Update mas.sh

--- install/mas/mas.sh
+++ install/mas/mas.sh
@@ -1,10 +1,7 @@
 echo "Installing applications..."
-mas install 406056744  # Evernote
 mas install 413744108  # Eon
-mas install 409789998  # Twitter
 mas install 405772121  # LittleIpsum
 mas install 588726889  # ReadKit
-mas install 949724400  # MoneyWiz
 mas install 1000397973 # Wallcat
 mas install 904280696  # Things
-mas install 1176895641 # Spark
+mas install 1059655371 # Newton
