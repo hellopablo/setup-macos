314d78bf04eed060a6e5878a71210d6b1e365687
Pablo de la Peña <p@shedcollective.org>
2019-06-17 10:29:17 +0100

    Update mas.sh

--- install/mas/mas.sh
+++ install/mas/mas.sh
@@ -5,3 +5,6 @@ mas install 588726889  # ReadKit
 mas install 1000397973 # Wallcat
 mas install 904280696  # Things
 mas install 1059655371 # Newton
+mas install 1273771160 # Kaleidoscope
+mas install 441258766  # Magnet
+mas install 1116599239 # NordVPN
